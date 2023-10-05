36593f92c856995656905ee56f94139fa0932b13
Jörn Lang <112864653+joern-lang-studitemps@users.noreply.github.com>
2023-10-05 09:44:09 +0000

    Add Graduate.t()

--- lib/yp_email_offers/graduate/graduate.ex
+++ lib/yp_email_offers/graduate/graduate.ex
@@ -1,6 +1,7 @@
 defmodule YPEmailOffers.Graduate do
   use Ecto.Schema
   import Ecto.Changeset
+  @type t :: %__MODULE__{}
 
   @primary_key {:id, :binary_id, autogenerate: true}
   schema "graduates" do
